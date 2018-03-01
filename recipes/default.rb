a5539ef7f55ecfb083882e6b8c05ef78e95cdcd0
Kirt Martin <kirt.martin@centroid.com>
2018-03-01 14:59:19 -0500

    initial git check-in

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,27 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+# Shutdown software services.
+
+# Determine OS Version
+
+# Store current OS Level information into log file prior to patching.
+# uname -a | tee -a os_level_before_2018Q1_patching
+# cat /etc/system-release | tee -a os_level_before_2018Q1_patching
+
+# Update the yum.repo file with the latest (create backup file)
+# /etc/yum.repos.d/yum.repo
+
+# Clear out yum cache
+# yum clean all
+
+# Check update file
+# yum check-update
+
+# Execute update of patching
+# yum update --exclude=kexec-tools* --exclude=system-config-kdump*
+
+# Reboot Server
+
+# Verify update
