99ba2685885ae4667f7f1382a57407b4028c7976
Kirt Martin <kirt.martin@centroid.com>
2018-03-01 14:57:14 -0500

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# darigold_linux_patch
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: darigold_linux_patch
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
